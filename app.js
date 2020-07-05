841688470bb699120e7480e6fd4439223addb9d8
FloricaAlin <FloricaAlin@users.noreply.github.com>
2020-07-05 11:55:12 +0300

    Created NextPlayer function. Created Hold Button logic.

--- app.js
+++ app.js
@@ -13,17 +13,49 @@ var scores = [0, 0];
 var roundScore = 0;
 var activePlayer = 0;
 
-//
-//document.querySelector('#current-'+activePlayer).innerHTML = '<em>'+ dice+'</em>';
 document.querySelector(".dice").style.display = "none";
-document.getElementById('score-0').textContent ='0';
-document.getElementById('score-1').textContent ='0';
-document.getElementById('current-0').textContent ='0';
-document.getElementById('current-1').textContent ='0';
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector(".btn-roll").addEventListener("click", function() {
+document.querySelector(".btn-roll").addEventListener("click", function () {
   var dice = Math.floor(Math.random() * 6) + 1;
   var diceDOM = document.querySelector(".dice");
   diceDOM.style.display = "block";
   diceDOM.src = "dice-" + dice + ".png";
+
+  //Update the round score if the rolled number is not 1
+
+  if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector("#current-" + activePlayer).textContent = roundScore;
+  } else {
+    //next player
+    NextPlayer();
+  }
 });
+
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  //Add Current score to Global score
+  scores[activePlayer] += roundScore;
+
+  //Update UI
+  document.querySelector("#score-" + activePlayer).textContent =
+    scores[activePlayer];
+
+  //Check if player won game
+
+  //Next player
+  NextPlayer();
+});
+
+function NextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+}
